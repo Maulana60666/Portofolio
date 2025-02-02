d76466678b0f1361a373789d8d9f145b6f591940
Maulana60666 <email@domain.com>
2025-02-02 19:46:05 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // Menambahkan efek smooth scroll pada klik link navbar
 document.querySelectorAll('nav ul li a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
@@ -8,14 +8,3 @@ document.querySelectorAll('nav ul li a').forEach(anchor => {
         });
     });
 });
-=======
-// Menambahkan efek smooth scroll pada klik link navbar
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
->>>>>>> 29195ba945bb2e9b6d0679eaa6c1bf4fd984ba88
