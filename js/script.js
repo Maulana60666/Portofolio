a66be23ad4352bd521402a58751aa538382c1ae4
maulana60666 <maulanaabdulfattah505@gmail.com>
2025-03-06 23:12:22 -0500

    80%

--- js/script.js
+++ js/script.js
@@ -7,3 +7,38 @@ document.querySelectorAll('nav ul li a').forEach(anchor => {
         });
     });
 });
+let slideIndex = [0, 0, 0]; // Simpan index gambar tiap project
+const slideContainers = document.querySelectorAll(".gallery-slide");
+
+
+function showSlide(projectIndex) {
+    const slides = slideContainers[projectIndex];
+    const totalSlides = slides.children.length;
+    if (slideIndex[projectIndex] >= totalSlides) slideIndex[projectIndex] = 0;
+    if (slideIndex[projectIndex] < 0) slideIndex[projectIndex] = totalSlides - 1;
+
+    const offset = -slideIndex[projectIndex] * 100; // Geser sejauh 100% tiap slide
+    slides.style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide(projectIndex) {
+    slideIndex[projectIndex]++;
+    showSlide(projectIndex);
+}
+
+function prevSlide(projectIndex) {
+    slideIndex[projectIndex]--;
+    showSlide(projectIndex);
+}
+
+
+
+function prevSlideCert(projectIndex) {
+    slideIndex[projectIndex]++;
+    showSlide(projectIndex);
+}
+
+function nextSlideCert(projectIndex) {
+    slideIndex[projectIndex]--;
+    showSlide(projectIndex);
+}
